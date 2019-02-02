c6da78ae25e9fed1b709a2075e1ff5741140951a
Daniel Pôrto Núñez <dani.nunez.dev@gmail.com>
2019-02-02 18:04:38 -0200

    Adaptações ES6

--- js/app.js
+++ js/app.js
@@ -1,47 +1,60 @@
 // Inimigos que nosso jogador deve evitar
-var Enemy = function() {
-    // As variáveis aplicadas a nossas instâncias entram aqui.
-    // Fornecemos uma a você para que possa começcar.
+class Enemy{
+	constructor(){
+		// As variáveis aplicadas a nossas instâncias entram aqui.
+	    // Fornecemos uma a você para que possa começcar.
 
-    // A imagem/sprite de nossos inimigos, isso usa um
-    // ajudante que é fornecido para carregar imagens
-    // com facilidade.
-    this.sprite = 'images/enemy-bug.png';
-};
+	    // A imagem/sprite de nossos inimigos, isso usa um
+	    // ajudante que é fornecido para carregar imagens
+	    // com facilidade.
+		this.sprite = 'images/enemy-bug.png';
+	}
+	// Atualize a posição do inimigo, método exigido pelo jogo
+	// Parâmetro: dt, um delta de tempo entre ticks
+	update(dt){
+		// Você deve multiplicar qualquer movimento pelo parâmetro
+	    // dt, o que garantirá que o jogo rode na mesma velocidade
+	    // em qualquer computador.
+	}
+	// Desenhe o inimigo na tela, método exigido pelo jogo
+	render(){
+		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+}
 
-// Atualize a posição do inimigo, método exigido pelo jogo
-// Parâmetro: dt, um delta de tempo entre ticks
-Enemy.prototype.update = function(dt) {
-    // Você deve multiplicar qualquer movimento pelo parâmetro
-    // dt, o que garantirá que o jogo rode na mesma velocidade
-    // em qualquer computador.
-};
+class Char{
+	constructor(){
+		this.sprite = 'images/char-boy.png';
+	}
 
-// Desenhe o inimigo na tela, método exigido pelo jogo
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+	update(){
 
-// Agora, escreva sua própria classe de jogador
-// Esta classe exige um método update(), 
-// um render() e um handleInput().
+	}
 
+	render(){
+		//ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	}
+
+	
+}
+
+let player = new Char(),
+	allEnemies = [];
+for(let i = 1; i <= 4; i++){
+	let enemy = new Enemy();
+	allEnemies.push(enemy);
+}
 
 // Represente seus objetos como instâncias.
 // Coloque todos os objetos inimgos numa array allEnemies
 // Coloque o objeto do jogador numa variável chamada jogador.
 
-
-
-// Isto reconhece cliques em teclas e envia as chaves para seu
-// jogador. método handleInput(). Não é preciso mudar nada.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+/*hotkeys('up,down,left,right', function(event,handler) {
+  switch(handler.key){
+	case "up":alert('you pressed up');break;
+	case "down":alert('you pressed down');break;
+	case "left":alert('you pressed left');break;
+	case "right":alert('you pressed right');break;
+ }
 });
+*/
