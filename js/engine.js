05da586562e0d18aa68dd81f5e4997b6e1f221c7
Daniel Pôrto Núñez <dani.nunez.dev@gmail.com>
2019-02-12 15:01:18 -0200

    Correção de pontuação para prestige

--- js/engine.js
+++ js/engine.js
@@ -127,7 +127,7 @@ var Engine = (function(global) {
             score.add();
             //Condição serve para não permitir que novas estrelas apareçam com uma no "bolso"
             //Também para bloquear o spawn de estralhas no ultimo prestige
-            if(score.points >= 99 && star.coletaStatus == false && star.prestiges <= 3){
+            if(score.points >= 999 && star.coletaStatus == false && star.prestiges <= 3){
                 star.setVisivel();
             }
         }
